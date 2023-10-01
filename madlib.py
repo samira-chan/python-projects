bd0a2fc06771e1473ff1090e006eafb416d3bca4
samira-chan <146542104+samira-chan@users.noreply.github.com>
2023-10-01 15:09:31 +0300

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,24 @@
+#string concatenation
+
+#youtuber = "SSamira"
+#print("subscribe to " + youtuber)
+#print("Subscribe {}".format(youtuber))
+#print(f"Subscribe to {youtuber}")
+
+food = input("Food: ")
+name = input("Name: ")
+adj= input("Adjective: ")
+animal = input("Animal: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+noun = input("Noun: ")
+
+madlibs = f"It was {food} day at school, and {name} was super {adj} for lunch. But when she went outside to eat, a {animal} stole her {food} ! \
+{name} chased the {animal} all over school. She {verb1} , {verb2} , and {verb3} through the playground. \
+Then she tripped on her {noun} and the {animal} escaped! \
+Luckily,{name} ’s friends were willing to share their {food} with her."
+
+print(madlibs)
+
+
